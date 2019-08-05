3c2f89a26dd1791dd68887647173e4023adc2d8c
Allan Denot <adenot@gmail.com>
2019-08-05 15:35:34 +1000

    Updating deployment percentage to allow only one openvpn task during deploy

--- ecs-service.tf
+++ ecs-service.tf
@@ -3,4 +3,7 @@ resource "aws_ecs_service" "default" {
   cluster         = "${var.cluster_name}"
   task_definition = "${aws_ecs_task_definition.default.arn}"
   desired_count   = 1
+
+  deployment_maximum_percent         = 100
+  deployment_minimum_healthy_percent = 0
 }
