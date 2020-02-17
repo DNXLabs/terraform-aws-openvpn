3b9d876eb5d9e6e4a574710d13f511d7b19bd32b
Arthur Diniz <arthurbdiniz@gmail.com>
2020-02-17 18:28:09 +1100

    Adds terraform 0.12 support
    Closes: #1
    
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- ecs-service.tf
+++ ecs-service.tf
@@ -1,7 +1,7 @@
 resource "aws_ecs_service" "default" {
   name            = "openvpn-${var.name}"
-  cluster         = "${var.cluster_name}"
-  task_definition = "${aws_ecs_task_definition.default.arn}"
+  cluster         = var.cluster_name
+  task_definition = aws_ecs_task_definition.default.arn
   desired_count   = 1
 
   deployment_maximum_percent         = 100
--- ecs-task-definition.tf
+++ ecs-task-definition.tf
@@ -1,8 +1,8 @@
 resource "aws_ecs_task_definition" "default" {
   family = "openvpn-${var.name}"
 
-  execution_role_arn = "${aws_iam_role.ecs_task.arn}"
-  task_role_arn      = "${aws_iam_role.ecs_task.arn}"
+  execution_role_arn = aws_iam_role.ecs_task.arn
+  task_role_arn      = aws_iam_role.ecs_task.arn
 
   volume {
     name      = "openvpn-${var.name}"
