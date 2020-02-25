2e311ca1d47f7ea3d8251591fae61183cfd69d59
Allan Denot <adenot@gmail.com>
2020-02-26 08:37:35 +1100

    Renaming security group to avoid conflict

--- sg.tf
+++ sg.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "openvpn" {
   count  = length(var.requester_cidrs) > 0 ? 1 : 0
-  name   = "openvpn-access"
+  name   = "openvpn-access-${var.name}"
   vpc_id = var.vpc_id
 
   ingress {
