a46ef340a6d7252543794a225ae047cb98b3a74f
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2021-02-19 20:30:02 +1100

    include hostame create

---
+++ route53.tf
@@ -0,0 +1,17 @@
+data "aws_route53_zone" "selected" {
+  count = var.hostname_create ? 1 : 0
+  name  = var.hosted_zone
+}
+
+resource "aws_route53_record" "hostname" {
+  count   = var.hostname_create ? 1 : 0
+  zone_id = data.aws_route53_zone.selected.*.zone_id[0]
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.ecs.dns_name
+    zone_id                = aws_lb.ecs.zone_id
+    evaluate_target_health = true
+  }
+}
