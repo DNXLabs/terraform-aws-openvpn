ed121f49ead93dc5e43fa469881ca1c79ba6ce97
Fabio Ramos <fabio.ramos@dnx.solutions>
2022-05-10 16:25:20 +1000

    add ignore public acls

--- s3.tf
+++ s3.tf
@@ -14,6 +14,8 @@ resource "aws_s3_bucket" "vpn" {
 resource "aws_s3_bucket_public_access_block" "vpn" {
   bucket = aws_s3_bucket.vpn.id
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
