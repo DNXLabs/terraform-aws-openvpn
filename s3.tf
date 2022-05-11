2e8e1657af88fef1bee74d215a92f98131672c41
Fabio Ramos <fabio.ramos@dnx.solutions>
2022-05-11 10:15:22 +1000

    modify KMS resource

--- s3.tf
+++ s3.tf
@@ -2,13 +2,6 @@ resource "aws_s3_bucket" "vpn" {
   bucket_prefix = "openvpn-${var.name}-"
   acl           = "private"
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
-      }
-    }
-  }
 }
 
 resource "aws_s3_bucket_public_access_block" "vpn" {
@@ -18,4 +11,15 @@ resource "aws_s3_bucket_public_access_block" "vpn" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "vpn" {
+  bucket = aws_s3_bucket.vpn.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = var.kms_key_arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
