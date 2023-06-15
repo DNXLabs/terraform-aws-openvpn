6bca44afc5c8509513056c76dcf7e20955bc3982
lucastercas <lucas.macedo@dnx.solutions>
2023-06-15 20:22:13 -0300

    Fix capacity provider to work with version

--- ecs.tf
+++ ecs.tf
@@ -3,12 +3,17 @@ resource "aws_ecs_cluster" "ecs" {
   depends_on = [aws_autoscaling_group.ecs, aws_ecs_capacity_provider.ecs_capacity_provider]
   name       = var.name
 
-  capacity_providers = ["${var.name}-capacity-provider"]
 
   lifecycle {
     ignore_changes = [
       tags
     ]
   }
+
+}
+
+resource "aws_ecs_cluster_capacity_providers" "ecs" {
+  cluster_name       = aws_ecs_cluster.ecs.name
+  capacity_providers = ["${var.name}-capacity-provider"]
 
 }
