eb8cc08938fd4e82bf31de92d0b34c7b66396eb4
Tiago Fortunato Lino <tiago.lino@dnx.solutions>
2023-10-05 10:21:18 +1100

    adjust bucket policy and capacity provier for AWS provider compatibility

--- ecs.tf
+++ ecs.tf
@@ -3,8 +3,6 @@ resource "aws_ecs_cluster" "ecs" {
   depends_on = [aws_autoscaling_group.ecs, aws_ecs_capacity_provider.ecs_capacity_provider]
   name       = var.name
 
-  capacity_providers = ["${var.name}-capacity-provider"]
-
   lifecycle {
     ignore_changes = [
       tags
@@ -12,3 +10,4 @@ resource "aws_ecs_cluster" "ecs" {
   }
 
 }
+
--- s3.tf
+++ s3.tf
@@ -3,13 +3,23 @@ resource "aws_s3_bucket" "vpn" {
 }
 
 resource "aws_s3_bucket_acl" "vpn" {
+  depends_on = [aws_s3_bucket_ownership_controls.vpn]
+
   bucket = aws_s3_bucket.vpn.id
   acl = "private"
 }
 
-resource "aws_s3_bucket_policy" "vpn" {
+resource "aws_s3_bucket_ownership_controls" "vpn" {
   bucket = aws_s3_bucket.vpn.id
-  policy        = var.s3_bucket_policy != "" ? var.s3_bucket_policy : null
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_policy" "vpn" {
+  count = var.s3_bucket_policy != "" ? 1 : 0
+  bucket = aws_s3_bucket.vpn.id
+  policy        = var.s3_bucket_policy
 }
 
 resource "aws_s3_bucket_public_access_block" "vpn" {
