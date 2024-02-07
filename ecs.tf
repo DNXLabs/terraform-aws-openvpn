422892d7c3b0af7c303effab3ba56d093f0399c0
Lucas de Macedo <122332519+lucasmacedot@users.noreply.github.com>
2024-02-07 18:52:33 -0400

    Add missing ecs_cluster_capacity_provider resource

--- ecs.tf
+++ ecs.tf
@@ -18,3 +18,7 @@ EOT
 
 }
 
+resource "aws_ecs_cluster_capacity_providers" "ecs" {
+  cluster_name       = aws_ecs_cluster.ecs.name
+  capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
+}
